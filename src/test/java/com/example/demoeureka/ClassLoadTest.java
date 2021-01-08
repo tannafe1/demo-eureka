b68d957b1d7d400047cba763e539e830b70b3c57
linxiansheng <215761699@qq.com>
2021-01-08 21:42:16 +0800

    test

--- src/test/java/com/example/demoeureka/ClassLoadTest.java
+++ src/test/java/com/example/demoeureka/ClassLoadTest.java
@@ -16,8 +16,10 @@ public class ClassLoadTest {
         System.out.println("test.y="+test.y);
         y++;
         System.out.println("test.y2="+test.y);
+        System.out.println("test.y2="+test.y);
         //test.x=1
         //test.y=0
         //test.y2=1
+        //sfjslfjsdfsfd
     }
 }
